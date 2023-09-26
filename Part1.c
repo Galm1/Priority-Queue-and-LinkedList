6b3610f31bc0fb1b2cc606f6de28521478ab8ddc
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-26 12:33:49 -0500

    test case passed

--- Part1.c
+++ Part1.c
@@ -2,34 +2,36 @@
 #include <stdlib.h>
 #define MAX 5  //is this nessasary? I dont know yet. We only want 5 user inputs, so maybe.
 
-struct node 
+struct node //building a node struct.
 {
 	int data;
 	struct node *next;
 };
 
-struct queue
+struct queue //building a struct for queue.
 {
 	struct node *head;
 	struct node *tail;
 };
 
-void enqueue(struct queue *q, int data){
-	struct node *new_node = malloc(sizeof(struct node));
-	new_node->data = data;
+void enqueue(struct queue *q, int data){ //enqueue takes a 'queue' struct pointer "q" and data.
+	struct node *new_node = malloc(sizeof(struct node)); //allocate needed memory for node new_node.
+	new_node->data = data; //set the new_node data and set its next to point to NULL, telling us it is last element.
 	new_node->next = NULL;
 	
 	if (q->tail == NULL){
 		q->head = new_node;
 		q->tail = new_node;
+		return;
 	} else {
-		q->head->next = new_node;
+		q->tail->next = new_node;
 		q->tail = new_node;
 	}
 }
 
 int dequeue(struct queue *q){
 	if (q->head == NULL){
+		printf("Queue is empty");
 		return -1; //telling us queue is empty
 	}
 	int data = q->head->data;
@@ -48,5 +50,14 @@ int dequeue(struct queue *q){
 
 int main()
 {
-	
+	struct queue q;
+	q.head = NULL;
+	q.tail = NULL;
+	enqueue(&q, 1);
+	enqueue(&q, 2);
+	enqueue(&q, 3);
+	printf("%d\n", dequeue(&q));
+	printf("%d\n", dequeue(&q));
+	printf("%d\n", dequeue(&q));
+	return 0;
 }
