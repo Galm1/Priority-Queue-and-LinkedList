71bdc1197d3712d4adb7b72fc1b442037efe1650
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-26 10:43:47 -0500

    dequeue added

--- Part1.c
+++ Part1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 5
+#define MAX 5  //is this nessasary? I dont know yet. We only want 5 user inputs, so maybe.
 
 struct node 
 {
@@ -18,6 +18,28 @@ void enqueue(struct queue *q, int data){
 	struct node *new_node = malloc(sizeof(struct node));
 	new_node->data = data;
 	new_node->next = NULL;
+	
+	if (q->tail == NULL){
+		q->head = new_node;
+		q->tail = new_node;
+	} else {
+		q->head->next = new_node;
+		q->tail = new_node;
+	}
+}
+
+int dequeue(struct queue *q){
+	if (q->head == NULL){
+		return -1; //telling us queue is empty
+	}
+	int data = q->head->data;
+	struct node *temp = q->head;
+	q->head = q->head->next;
+	if(q->head == NULL){
+		q->tail = NULL;
+	}
+	free(temp);
+	return data;
 }
 
 
